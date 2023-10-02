c1cc26cd12540781d542df33a52fee740be33c88
aguillau <guillaume.antier@ynov.com>
2023-10-02 09:12:24 +0200

    Ajout des fonction randomWord et displayWord

--- hangman.go
+++ hangman.go
@@ -3,6 +3,8 @@ package main
 import (
     "fmt"
     "io/ioutil"
+	"math/rand"
+	"time"
 )
 
 func loadWords() []string {
@@ -13,17 +15,37 @@ func loadWords() []string {
 		panic(err)
 	}
 	for _,char := range data {
-		inter = inter + string(char)
 		if char == '\n' {
 			arrayOfWords = append(arrayOfWords, inter)
 			inter = ""
+			continue
 		}
+		inter = inter + string(char)
 	}
-	arrayOfWords = append(arrayOfWords, inter)
 	return arrayOfWords
+}
+
+func randomWord(words []string) string {
+	rand.Seed(time.Now().UnixNano())
+	random := rand.Intn(len(words))
+	return words[random]
+}
+
+func displayWord(word string) string {
+	display := ""
+	for i := 0; i < len(word)-1; i++ {
+		display = display + "_"
+	}
+	for i := 0; i < len(word)/2-1; i++ {
+		randomNumber := rand.Intn(len(word)-1)
+		display = display[:randomNumber] + string(word[randomNumber]) + display[randomNumber+1:]
+	}
+	return display
 }
 
 func main() {
 	words := loadWords()
-	fmt.Println(words)
+	word := randomWord(words)
+	display := displayWord(word)
+	fmt.Println(display)
 }
