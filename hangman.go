4e046ce5e33141af6143faa8368f6dc70c8e6bb8
lnoe <noe.lechat@ynov.com>
2023-10-09 09:04:18 +0200

    Debug

--- hangman.go
+++ hangman.go
@@ -13,20 +13,22 @@ import ( // Import of the packages
 func menu() { // Display of command menu
 	reader := bufio.NewReader(os.Stdin)
 	spaces := strings.Repeat(" ", 50)
+	clear := "\033[H\033[2J"
 	red := "\033[31m"
 	blue := "\033[34m"
 	black := "\033[30m"
 	magenta := "\033[35m"
 	cyan := "\033[36m"
 	green := "\033[32m"
+	yellow := "\033[33m"
 	reset_color := "\033[0m"
 	for { //Define the principal menu
-		fmt.Println(spaces, "                       \033[35m||\033[0m                       ")
-		fmt.Println(spaces, "                       \033[35m||\033[0m                       ")
+		fmt.Println(spaces, "                       "+magenta+"||"+reset_color+"                       ")
+		fmt.Println(spaces, "                       "+magenta+"||"+reset_color+"                       ")
 		fmt.Println(spaces, magenta, "----------------------Menu----------------------", reset_color)
-		fmt.Println(spaces, magenta, "|\033[0m    [1] : Language                            \033[35m|", reset_color)
-		fmt.Println(spaces, magenta, "|\033[0m    [2] : Others                              \033[35m|", reset_color)
-		fmt.Println(spaces, magenta, "|\033[0m    [3] : Leave the game                      \033[35m|", reset_color)
+		fmt.Println(spaces, magenta, "|"+reset_color+"    [1] : Language                            "+magenta+"|", reset_color)
+		fmt.Println(spaces, magenta, "|"+reset_color+"    [2] : Others                              "+magenta+"|", reset_color)
+		fmt.Println(spaces, magenta, "|"+reset_color+"    [3] : Leave the game                      "+magenta+"|", reset_color)
 		fmt.Println(spaces, magenta, "------------------------------------------------", reset_color)
 		fmt.Println(spaces, "\033[3mCreated by Guillaume, Arthur and Noé", reset_color)
 		fmt.Println()
@@ -34,28 +36,28 @@ func menu() { // Display of command menu
 		fmt.Print(spaces, " Choose an option: ")
 		option, _ := reader.ReadString('\n')
 		option = strings.TrimSpace(option)
-		fmt.Print("\033[H\033[2J")
+		fmt.Print(clear)
 		switch option {
 		case "1":
 			finish := false
 			for !finish { // Define the submenu named "Language"
-				fmt.Println(spaces, "                         \033[35m||\033[0m                       ")
-				fmt.Println(spaces, "                         \033[35m||\033[0m                       ")
+				fmt.Println(spaces, "                         "+magenta+"||"+reset_color+"                       ")
+				fmt.Println(spaces, "                         "+magenta+"||"+reset_color+"                       ")
 				fmt.Println(spaces, magenta+"----------------------Language----------------------"+reset_color)
-				fmt.Println(spaces, magenta+"|\033[0m    [1] : Play to hangman in "+cyan+"fr"+reset_color+"en"+red+"ch"+reset_color+"               \033[35m|"+reset_color)
-				fmt.Println(spaces, magenta+"|\033[0m    [2] : Play to hangman in "+red+"en"+reset_color+"gli"+blue+"sh"+reset_color+"              \033[35m|"+reset_color)
-				fmt.Println(spaces, magenta+"|\033[0m    [3] : Play to hangman in "+green+"it"+reset_color+"ali"+red+"an"+reset_color+"              \033[35m|"+reset_color)
-				fmt.Println(spaces, magenta+"|\033[0m    [4] : Play to hangman in "+red+"sp"+reset_color+"\033[33mani"+reset_color+red+"sh"+reset_color+"              \033[35m|", reset_color)
-				fmt.Println(spaces, magenta+"|\033[0m    [5] : Play to hangman in "+red+"por"+reset_color+green+"tugu"+reset_color+red+"ese"+reset_color+"           \033[35m|", reset_color)
-				fmt.Println(spaces, magenta+"|\033[0m    [6] : Play to hangman in "+black+"ge"+reset_color+red+"rm"+reset_color+"\033[33man"+reset_color+"               \033[35m|", reset_color)
-				fmt.Println(spaces, magenta+"|\033[0m    [7] : Back to menu                            \033[35m|"+reset_color)
-				fmt.Println(spaces, magenta+"|\033[0m    [8] : Leave the game                          \033[35m|"+reset_color)
+				fmt.Println(spaces, magenta+"|"+reset_color+"    [1] : Play to hangman in "+cyan+"fr"+reset_color+"en"+red+"ch"+reset_color+"               "+magenta+"|"+reset_color)
+				fmt.Println(spaces, magenta+"|"+reset_color+"    [2] : Play to hangman in "+red+"en"+reset_color+"gli"+blue+"sh"+reset_color+"              "+magenta+"|"+reset_color)
+				fmt.Println(spaces, magenta+"|"+reset_color+"    [3] : Play to hangman in "+green+"it"+reset_color+"ali"+red+"an"+reset_color+"              "+magenta+"|"+reset_color)
+				fmt.Println(spaces, magenta+"|"+reset_color+"    [4] : Play to hangman in "+red+"sp"+reset_color+yellow+"ani"+reset_color+red+"sh"+reset_color+"              "+magenta+"|", reset_color)
+				fmt.Println(spaces, magenta+"|"+reset_color+"    [5] : Play to hangman in "+red+"por"+reset_color+green+"tugu"+reset_color+red+"ese"+reset_color+"           "+magenta+"|", reset_color)
+				fmt.Println(spaces, magenta+"|"+reset_color+"    [6] : Play to hangman in "+black+"ge"+reset_color+red+"rm"+reset_color+yellow+"an"+reset_color+"               "+magenta+"|", reset_color)
+				fmt.Println(spaces, magenta+"|"+reset_color+"    [7] : Back to menu                            "+magenta+"|"+reset_color)
+				fmt.Println(spaces, magenta+"|"+reset_color+"    [8] : Leave the game                          "+magenta+"|"+reset_color)
 				fmt.Println(spaces, magenta+"----------------------------------------------------"+reset_color)
 				fmt.Print(spaces, " Choose an option: ")
 
 				option, _ := reader.ReadString('\n')
 				option = strings.TrimSpace(option)
-				fmt.Print("\033[H\033[2J")
+				fmt.Print(clear)
 				life := 10
 				failed_letter := ""
 				indexHangman := 0
@@ -64,7 +66,7 @@ func menu() { // Display of command menu
 					words := loadWords("base_de_donnée/words.txt")
 					word := randomWord(words)
 					display := displayWord(word)
-					game_mode := "Game mode : \033[36mfr" + reset_color + "en" + red + "ch\033[0m words"
+					game_mode := "Game mode : \033[36mfr" + reset_color + "en" + red + "ch" + reset_color + " words"
 					fmt.Println(spaces, game_mode)
 					fmt.Println(spaces, "Good Luck, you have 10 attemps.")
 					for !wordFind(word, display) && life > 0 {
@@ -72,14 +74,14 @@ func menu() { // Display of command menu
 						display, life, indexHangman, failed_letter = askUser(display, word, life, indexHangman, failed_letter, game_mode)
 					}
 					if life == 0 {
-						fmt.Print("\033[H\033[2J")
+						fmt.Print(clear)
 						fmt.Println(spaces, "You lose, the good words was : ", word)
 					}
 				case "2":
 					words := loadWords("base_de_donnée/english.txt")
 					word := randomWord(words)
 					display := displayWord(word)
-					game_mode := "Game mode : \033[31men" + reset_color + "gli" + blue + "sh\033[0m words"
+					game_mode := "Game mode : \033[31men" + reset_color + "gli" + blue + "sh" + reset_color + " words"
 					fmt.Println(spaces, game_mode)
 					fmt.Println(spaces, "Good Luck, you have 10 attemps.")
 					for !wordFind(word, display) && life > 0 {
@@ -87,14 +89,14 @@ func menu() { // Display of command menu
 						display, life, indexHangman, failed_letter = askUser(display, word, life, indexHangman, failed_letter, game_mode)
 					}
 					if life == 0 {
-						fmt.Print("\033[H\033[2J")
+						fmt.Print(clear)
 						fmt.Println(spaces, "You lose, the good words was : ", word)
 					}
 				case "3":
 					words := loadWords("base_de_donnée/italiano.txt")
 					word := randomWord(words)
 					display := displayWord(word)
-					game_mode := "Game mode : \033[32mit" + reset_color + "ali" + red + "an\033[0m words"
+					game_mode := "Game mode : \033[32mit" + reset_color + "ali" + red + "an" + reset_color + " words"
 					fmt.Println(spaces, game_mode)
 					fmt.Println(spaces, "Good Luck, you have 10 attemps.")
 					for !wordFind(word, display) && life > 0 {
@@ -102,14 +104,14 @@ func menu() { // Display of command menu
 						display, life, indexHangman, failed_letter = askUser(display, word, life, indexHangman, failed_letter, game_mode)
 					}
 					if life == 0 {
-						fmt.Print("\033[H\033[2J")
+						fmt.Print(clear)
 						fmt.Println(spaces, "You lose, the good words was : ", word)
 					}
 				case "4":
 					words := loadWords("base_de_donnée/espanol.txt")
 					word := randomWord(words)
 					display := displayWord(word)
-					game_mode := "Game mode : \033[31msp" + reset_color + "\033[33mani" + reset_color + red + "sh" + reset_color + " words"
+					game_mode := "Game mode : \033[31msp" + reset_color + yellow + "ani" + reset_color + red + "sh" + reset_color + " words"
 					fmt.Println(spaces, game_mode)
 					fmt.Println(spaces, "Good Luck, you have 10 attemps.")
 					for !wordFind(word, display) && life > 0 {
@@ -117,7 +119,7 @@ func menu() { // Display of command menu
 						display, life, indexHangman, failed_letter = askUser(display, word, life, indexHangman, failed_letter, game_mode)
 					}
 					if life == 0 {
-						fmt.Print("\033[H\033[2J")
+						fmt.Print(clear)
 						fmt.Println(spaces, "You lose, the good words was : ", word)
 					}
 				case "5":
@@ -132,14 +134,14 @@ func menu() { // Display of command menu
 						display, life, indexHangman, failed_letter = askUser(display, word, life, indexHangman, failed_letter, game_mode)
 					}
 					if life == 0 {
-						fmt.Print("\033[H\033[2J")
+						fmt.Print(clear)
 						fmt.Println(spaces, "You lose, the good words was : ", word)
 					}
 				case "6":
 					words := loadWords("base_de_donnée/allemand.txt")
 					word := randomWord(words)
 					display := displayWord(word)
-					game_mode := "Game mode : \033[30mge" + reset_color + red + "rm" + reset_color + "\033[33man" + reset_color
+					game_mode := "Game mode : \033[30mge" + reset_color + red + "rm" + reset_color + yellow + "an" + reset_color
 					fmt.Println(spaces, game_mode)
 					fmt.Println(spaces, "Good Luck, you have 10 attemps.")
 					for !wordFind(word, display) && life > 0 {
@@ -147,21 +149,21 @@ func menu() { // Display of command menu
 						display, life, indexHangman, failed_letter = askUser(display, word, life, indexHangman, failed_letter, game_mode)
 					}
 					if life == 0 {
-						fmt.Print("\033[H\033[2J")
+						fmt.Print(clear)
 						fmt.Println(spaces, "You lose, the good words was : ", word)
 					}
 				case "8":
-					fmt.Print("\033[H\033[2J")
+					fmt.Print(clear)
 					content, _ := ioutil.ReadFile("affichage/goodbye.txt")
 					fmt.Println(string(content))
 					time.Sleep(3 * time.Second)
-					fmt.Print("\033[H\033[2J")
+					fmt.Print(clear)
 					os.Exit(0)
 				case "7":
-					fmt.Print("\033[H\033[2J")
+					fmt.Print(clear)
 					finish = true
 				default:
-					fmt.Print("\033[H\033[2J")
+					fmt.Print(clear)
 					fmt.Println(spaces, "Invalid option")
 				}
 			}
@@ -169,24 +171,24 @@ func menu() { // Display of command menu
 		case "2":
 			finish := false
 			for !finish { // Define the submenu "Others"
-				fmt.Println(spaces, "                        \033[35m||\033[0m                        ")
-				fmt.Println(spaces, "                        \033[35m||\033[0m                        ")
+				fmt.Println(spaces, "                        "+magenta+"||"+reset_color+"                        ")
+				fmt.Println(spaces, "                        "+magenta+"||"+reset_color+"                        ")
 				fmt.Println(spaces, magenta+"----------------------Others----------------------"+reset_color)
-				fmt.Println(spaces, magenta+"|\033[0m    [1] : Play to hangman with french citys     \033[35m|"+reset_color)
-				fmt.Println(spaces, magenta+"|\033[0m    [2] : Play to hangman with countrys         \033[35m|"+reset_color)
-				fmt.Println(spaces, magenta+"|\033[0m    [3] : Play to hangman with capitals         \033[35m|"+reset_color)
-				fmt.Println(spaces, magenta+"|\033[0m    [4] : Play to hangman with sports           \033[35m|"+reset_color)
-				fmt.Println(spaces, magenta+"|\033[0m    [5] : Play to hangman with brands           \033[35m|"+reset_color)
-				fmt.Println(spaces, magenta+"|\033[0m    [6] : Play to hangman with foods            \033[35m|"+reset_color)
-				fmt.Println(spaces, magenta+"|\033[0m    [7] : Play to hangman with drinks           \033[35m|"+reset_color)
-				fmt.Println(spaces, magenta+"|\033[0m    [8] : Back to menu                          \033[35m|"+reset_color)
-				fmt.Println(spaces, magenta+"|\033[0m    [9] : Leave the game                        \033[35m|"+reset_color)
+				fmt.Println(spaces, magenta+"|"+reset_color+"    [1] : Play to hangman with french citys     "+magenta+"|"+reset_color)
+				fmt.Println(spaces, magenta+"|"+reset_color+"    [2] : Play to hangman with countrys         "+magenta+"|"+reset_color)
+				fmt.Println(spaces, magenta+"|"+reset_color+"    [3] : Play to hangman with capitals         "+magenta+"|"+reset_color)
+				fmt.Println(spaces, magenta+"|"+reset_color+"    [4] : Play to hangman with sports           "+magenta+"|"+reset_color)
+				fmt.Println(spaces, magenta+"|"+reset_color+"    [5] : Play to hangman with brands           "+magenta+"|"+reset_color)
+				fmt.Println(spaces, magenta+"|"+reset_color+"    [6] : Play to hangman with foods            "+magenta+"|"+reset_color)
+				fmt.Println(spaces, magenta+"|"+reset_color+"    [7] : Play to hangman with drinks           "+magenta+"|"+reset_color)
+				fmt.Println(spaces, magenta+"|"+reset_color+"    [8] : Back to menu                          "+magenta+"|"+reset_color)
+				fmt.Println(spaces, magenta+"|"+reset_color+"    [9] : Leave the game                        "+magenta+"|"+reset_color)
 				fmt.Println(spaces, magenta+"--------------------------------------------------"+reset_color)
 				fmt.Print(spaces, " Choose an option: ")
 
 				option, _ := reader.ReadString('\n')
 				option = strings.TrimSpace(option)
-				fmt.Print("\033[H\033[2J")
+				fmt.Print(clear)
 				life := 10
 				failed_letter := ""
 				indexHangman := 0
@@ -203,7 +205,7 @@ func menu() { // Display of command menu
 						display, life, indexHangman, failed_letter = askUser(display, word, life, indexHangman, failed_letter, game_mode)
 					}
 					if life == 0 {
-						fmt.Print("\033[H\033[2J")
+						fmt.Print(clear)
 						fmt.Println(spaces, "You lose, the good words was : ", word)
 					}
 				case "2":
@@ -218,7 +220,7 @@ func menu() { // Display of command menu
 						display, life, indexHangman, failed_letter = askUser(display, word, life, indexHangman, failed_letter, game_mode)
 					}
 					if life == 0 {
-						fmt.Print("\033[H\033[2J")
+						fmt.Print(clear)
 						fmt.Println(spaces, "You lose, the good words was : ", word)
 					}
 				case "3":
@@ -233,7 +235,7 @@ func menu() { // Display of command menu
 						display, life, indexHangman, failed_letter = askUser(display, word, life, indexHangman, failed_letter, game_mode)
 					}
 					if life == 0 {
-						fmt.Print("\033[H\033[2J")
+						fmt.Print(clear)
 						fmt.Println(spaces, "You lose, the good words was : ", word)
 					}
 				case "4":
@@ -248,7 +250,7 @@ func menu() { // Display of command menu
 						display, life, indexHangman, failed_letter = askUser(display, word, life, indexHangman, failed_letter, game_mode)
 					}
 					if life == 0 {
-						fmt.Print("\033[H\033[2J")
+						fmt.Print(clear)
 						fmt.Println(spaces, "You lose, the good words was : ", word)
 					}
 				case "5":
@@ -263,7 +265,7 @@ func menu() { // Display of command menu
 						display, life, indexHangman, failed_letter = askUser(display, word, life, indexHangman, failed_letter, game_mode)
 					}
 					if life == 0 {
-						fmt.Print("\033[H\033[2J")
+						fmt.Print(clear)
 						fmt.Println(spaces, "You lose, the good words was : ", word)
 					}
 				case "6":
@@ -278,7 +280,7 @@ func menu() { // Display of command menu
 						display, life, indexHangman, failed_letter = askUser(display, word, life, indexHangman, failed_letter, game_mode)
 					}
 					if life == 0 {
-						fmt.Print("\033[H\033[2J")
+						fmt.Print(clear)
 						fmt.Println(spaces, "You lose, the good words was : ", word)
 					}
 				case "7":
@@ -293,33 +295,33 @@ func menu() { // Display of command menu
 						display, life, indexHangman, failed_letter = askUser(display, word, life, indexHangman, failed_letter, game_mode)
 					}
 					if life == 0 {
-						fmt.Print("\033[H\033[2J")
+						fmt.Print(clear)
 						fmt.Println(spaces, "You lose, the good words was : ", word)
 					}
 				case "9":
-					fmt.Print("\033[H\033[2J")
+					fmt.Print(clear)
 					content, _ := ioutil.ReadFile("affichage/goodbye.txt")
 					fmt.Println(string(content))
 					time.Sleep(3 * time.Second)
-					fmt.Print("\033[H\033[2J")
+					fmt.Print(clear)
 					os.Exit(0)
 				case "8":
-					fmt.Print("\033[H\033[2J")
+					fmt.Print(clear)
 					finish = true
 				default:
-					fmt.Print("\033[H\033[2J")
+					fmt.Print(clear)
 					fmt.Println(spaces, "Invalid option")
 				}
 			}
 		case "3":
-			fmt.Print("\033[H\033[2J")
+			fmt.Print(clear)
 			content, _ := ioutil.ReadFile("affichage/goodbye.txt")
 			fmt.Println(string(content))
 			time.Sleep(3 * time.Second)
-			fmt.Print("\033[H\033[2J")
+			fmt.Print(clear)
 			os.Exit(0)
 		default:
-			fmt.Print("\033[H\033[2J")
+			fmt.Print(clear)
 			fmt.Println(spaces, "Invalid option")
 
 		}
@@ -369,6 +371,7 @@ func displayWord(word string) string { // Function who create the game, we displ
 	return display
 }
 func askUser(display string, word string, life int, indexHangman int, failed_letter string, game_mode string) (string, int, int, string) { // Function who verify if there is an error when the user give him a letter
+	clear := "\033[H\033[2J"
 	var input string
 	spaces := strings.Repeat(" ", 50)
 	fmt.Print(spaces, " Choose : ")
@@ -379,7 +382,7 @@ func askUser(display string, word string, life int, indexHangman int, failed_let
 		fmt.Print(spaces, " Choose : ")
 		fmt.Scanln(&input)
 	}
-	fmt.Print("\033[H\033[2J")
+	fmt.Print(clear)
 	display, life, indexHangman, failed_letter = isPresent(strings.ToUpper(input), word, display, life, indexHangman, failed_letter, game_mode)
 	return display, life, indexHangman, failed_letter
 }
@@ -437,15 +440,16 @@ func isPresent(letter string, word string, display string, life int, indexHangma
 }
 
 func wordFind(word string, display string) bool { // Function who verify if the user have find the word and display the message of congrats
+	clear := "\033[H\033[2J"
 	spaces := strings.Repeat(" ", 50)
 	if !contains(display, "_") {
 		fmt.Println(spaces, display)
-		fmt.Print("\033[H\033[2J")
+		fmt.Print(clear)
 		content, _ := ioutil.ReadFile("affichage/congrats.txt")
 		fmt.Println(spaces, string(content))
 		fmt.Println(spaces, "You find the word: ", word)
 		time.Sleep(5 * time.Second)
-		fmt.Print("\033[H\033[2J")
+		fmt.Print(clear)
 		return true
 	}
 	return false
@@ -480,6 +484,7 @@ func displayHangman(life int, indexHangman int) int { // Function who display a 
 }
 
 func main() {
-	fmt.Print("\033[H\033[2J")
+	clear := "\033[H\033[2J"
+	fmt.Print(clear)
 	menu()
 }
