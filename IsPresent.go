443ff5c32febe7d585d0af68c7577b7dd31cf4b0
lnoe <noe.lechat@ynov.com>
2023-10-11 09:18:37 +0200

    Ajout de la fonction contains dans le fichier IsPresent

--- IsPresent.go
+++ IsPresent.go
@@ -56,3 +56,12 @@ func IsPresent(letter string, word string, display string, life int, indexHangma
 		return display, life, indexHangman, failed_letter
 	}
 }
+
+func Contains(inter string, x string) bool { // Function who test if a string is present on another string
+	for i := 0; i < len(inter); i++ {
+		if x == string(inter[i]) {
+			return true
+		}
+	}
+	return false
+}
