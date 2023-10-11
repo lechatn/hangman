07dd00765b2c552e133f692c9372135b5f84dfd2
lnoe <noe.lechat@ynov.com>
2023-10-11 08:46:55 +0200

    Sorti fonction ispresent du fichier principal

---
+++ IsPresent.go
@@ -0,0 +1,58 @@
+package hangman
+
+import (
+	"fmt"
+	"strings"
+)
+
+func IsPresent(letter string, word string, display string, life int, indexHangman int, failed_letter string, game_mode string) (string, int, int, string) { // Function who verify if the letter is present in the word
+	spaces := strings.Repeat(" ", 50)
+	isFind := false
+	for i, char := range word {
+		if i == len(word)-1 && string(char) == letter {
+			display = display[:i] + letter
+			isFind = true
+			continue
+		}
+		if string(char) == letter {
+			display = display[:i] + letter + display[i+1:]
+			isFind = true
+		}
+	}
+	if !isFind { // if the letter is not in the word, we said this to the user and display the next part of José
+		if Contains(failed_letter, letter) {
+			fmt.Println(spaces, game_mode)
+			fmt.Println(spaces, "Not present in the word", life, "attemps remaining")
+			fmt.Println(spaces, "You already tried this letter")
+			fmt.Println(spaces, "False letters you have already tried: ", failed_letter) //	We remind to the user the false letters that he already tried
+			if life <= 10 {
+				indexHangman = DisplayHangman(life, indexHangman)
+			}
+			return display, life, indexHangman, failed_letter
+		}
+		if !Contains(failed_letter, letter) {
+			if failed_letter == "" {
+				failed_letter = failed_letter + letter
+			} else {
+				failed_letter = failed_letter + "," + letter
+			}
+		}
+		life -= 1
+		fmt.Println(spaces, game_mode)
+		fmt.Println(spaces, "Not present in the word", life, "attemps remaining")
+		fmt.Println(spaces, "False letters you have already tried: ", failed_letter)
+		if life < 10 {
+			indexHangman += 7
+			indexHangman = DisplayHangman(life, indexHangman)
+		}
+		return display, life, indexHangman, failed_letter
+	} else { // if the letter is in the word, we said this to the user and we display the present part of José
+		fmt.Println(spaces, game_mode)
+		fmt.Println(spaces, "Present in the word", life, "attemps remaining")
+		fmt.Println(spaces, "Right letters you have already tried: ", failed_letter)
+		if life <= 10 {
+			indexHangman = DisplayHangman(life, indexHangman)
+		}
+		return display, life, indexHangman, failed_letter
+	}
+}
