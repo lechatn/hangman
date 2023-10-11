cc9b2a9a31cd0faf7116ed6e3e14e2334bfb219c
lnoe <noe.lechat@ynov.com>
2023-10-11 08:45:57 +0200

    Sorti fonction ispresent du fichier principal

--- main/hangman.go
+++ main/hangman.go
@@ -462,60 +462,8 @@ func askUser(display string, word string, life int, indexHangman int, failed_let
 		fmt.Scanln(&input)
 	}
 	fmt.Print(clear)
-	display, life, indexHangman, failed_letter = isPresent(strings.ToUpper(input), word, display, life, indexHangman, failed_letter, game_mode)
+	display, life, indexHangman, failed_letter = hangman.IsPresent(strings.ToUpper(input), word, display, life, indexHangman, failed_letter, game_mode)
 	return display, life, indexHangman, failed_letter
-}
-
-func isPresent(letter string, word string, display string, life int, indexHangman int, failed_letter string, game_mode string) (string, int, int, string) { // Function who verify if the letter is present in the word
-	spaces := strings.Repeat(" ", 50)
-	isFind := false
-	for i, char := range word {
-		if i == len(word)-1 && string(char) == letter {
-			display = display[:i] + letter
-			isFind = true
-			continue
-		}
-		if string(char) == letter {
-			display = display[:i] + letter + display[i+1:]
-			isFind = true
-		}
-	}
-	if !isFind { // if the letter is not in the word, we said this to the user and display the next part of José
-		if hangman.Contains(failed_letter, letter) {
-			fmt.Println(spaces, game_mode)
-			fmt.Println(spaces, "Not present in the word", life, "attemps remaining")
-			fmt.Println(spaces, "You already tried this letter")
-			fmt.Println(spaces, "False letters you have already tried: ", failed_letter) //	We remind to the user the false letters that he already tried
-			if life <= 10 {
-				indexHangman = hangman.DisplayHangman(life, indexHangman)
-			}
-			return display, life, indexHangman, failed_letter
-		}
-		if !hangman.Contains(failed_letter, letter) {
-			if failed_letter == "" {
-				failed_letter = failed_letter + letter
-			} else {
-				failed_letter = failed_letter + "," + letter
-			}
-		}
-		life -= 1
-		fmt.Println(spaces, game_mode)
-		fmt.Println(spaces, "Not present in the word", life, "attemps remaining")
-		fmt.Println(spaces, "False letters you have already tried: ", failed_letter)
-		if life < 10 {
-			indexHangman += 7
-			indexHangman = hangman.DisplayHangman(life, indexHangman)
-		}
-		return display, life, indexHangman, failed_letter
-	} else { // if the letter is in the word, we said this to the user and we display the present part of José
-		fmt.Println(spaces, game_mode)
-		fmt.Println(spaces, "Present in the word", life, "attemps remaining")
-		fmt.Println(spaces, "Right letters you have already tried: ", failed_letter)
-		if life <= 10 {
-			indexHangman = hangman.DisplayHangman(life, indexHangman)
-		}
-		return display, life, indexHangman, failed_letter
-	}
 }
 
 func main() { // We define the main function who launch the game
