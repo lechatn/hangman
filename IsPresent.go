1c957444182ce7ff526b08b59cdc73e3637bdd3c
lechatn <noe.lechat@ynov.com>
2023-11-13 08:20:06 +0100

    Modif ispresent

--- IsPresent.go
+++ IsPresent.go
@@ -1,12 +1,9 @@
 package hangman
 
 import (
-	"fmt"
-	"strings"
 )
 
 func IsPresent(letter string, word string, display string, life int, indexHangman int, failed_letter string) (string, int, int, string) { // Function who verify if the letter is present in the word
-	spaces := strings.Repeat(" ", 50)
 	isFind := false
 	for i, char := range word {
 		if i == len(word)-1 && string(char) == letter {
@@ -21,9 +18,6 @@ func IsPresent(letter string, word string, display string, life int, indexHangma
 	}
 	if !isFind { // if the letter is not in the word, we said this to the user and display the next part of José
 		if Contains(failed_letter, letter) {
-			fmt.Println(spaces, "Not present in the word", life, "attemps remaining")
-			fmt.Println(spaces, "You already tried this letter")
-			fmt.Println(spaces, "False letters you have already tried: ", failed_letter) //	We remind to the user the false letters that he already tried
 			if life <= 10 {
 				indexHangman = DisplayHangman(life, indexHangman)
 			}
@@ -37,16 +31,12 @@ func IsPresent(letter string, word string, display string, life int, indexHangma
 			}
 		}
 		life -= 1
-		fmt.Println(spaces, "Not present in the word", life, "attemps remaining")
-		fmt.Println(spaces, "False letters you have already tried: ", failed_letter)
 		if life < 10 {
 			indexHangman += 7
 			indexHangman = DisplayHangman(life, indexHangman)
 		}
 		return display, life, indexHangman, failed_letter
 	} else { // if the letter is in the word, we said this to the user and we display the present part of José
-		fmt.Println(spaces, "Present in the word", life, "attemps remaining")
-		fmt.Println(spaces, "Right letters you have already tried: ", failed_letter)
 		if life <= 10 {
 			indexHangman = DisplayHangman(life, indexHangman)
 		}
